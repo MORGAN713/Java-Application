e2efa7b9b4fc4b13fea0fd7ff81b446ea26d65a6
MORGAN713 <morganazubuike@gmail>
2025-10-23 12:02:14 +0100

    javaKeywords

---
+++ TypeCasting.java
@@ -0,0 +1,15 @@
+public class TypeCasting{
+	public static void main (String [] args){
+		//widening
+		double num = 10;
+		
+		System.out.printf("the value is %f",num);
+		
+		//narrowing
+		double price = 32.89;
+		int result  = (int) price;
+		System.out.printf(" the value is %d%n",result);
+		
+	}
+	
+}
