616d7eb145c7d3433d2a0e6b74a4a37bc9cded76
MORGAN713 <morganazubuike@gmail>
2025-10-28 12:02:36 +0100

    operator part 1

--- Operatorspart1.java
+++ Operatorspart1.java
@@ -2,6 +2,8 @@ public class OperatorsPart1{
 	public static void main(String[] args){
 		int num = 50;
 		
+		//Compound 
+		
 		num += 8;
 		System.out.printf("The value of num is %d%n",num);
 		
@@ -17,7 +19,40 @@ public class OperatorsPart1{
 		num %= 3;
 		System.out.printf("The value of num is %d%n",num);
 		
+		//Arithmetic OPerator
+		int num1 = 10;
+		int num2 = 4;
+		String name = "morgan";
 		
+		int sum = num1 + num2;
+		int product = num1 * num2;
+		int minus = num1 - num2;
+		double divide = (double)num1/num2;
+		int modulos = num1%num2;
+		
+		System.out.printf("Sum is %d%n",sum);
+		System.out.printf("product is %d%n",product);
+		System.out.printf("Difference btw num1 and num2 is %d%n",minus);
+		System.out.printf("Result is %f%n",divide);
+		System.out.printf("Modulus is %d%n",modulos);
+		System.out.println(name + sum);
+		
+		//Relational Operators
+		int firstNum = 12;
+		int secondNum = 15;
+		boolean isGreater = firstNum > secondNum;
+		boolean isLesser = firstNum < secondNum;
+		boolean isGreaterThanOrEqualTo = firstNum >= secondNum;
+		boolean isLessThanOrEqualTo = firstNum <= secondNum;
+		boolean isNotEqualTo = firstNum != secondNum;
+		boolean isEqualTo = firstNum == secondNum;
+		
+		System.out.printf("Is firstNum > secondNum? %b%n",isGreater);
+		System.out.printf("Is firstNum < secondNum? %b%n",isLesser);
+		System.out.printf("Is firstNum >= secondNum? %b%n",isGreaterThanOrEqualTo);
+		System.out.printf("Is firstNum <= secondNum? %b%n",isLessThanOrEqualTo);
+		System.out.printf("Is firstNum != secondNum? %b%n",isNotEqualTo);
+		System.out.printf("Is firstNum == secondNum? %b%n",isEqualTo);
 		
 	}
 	
