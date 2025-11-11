a5ce71d58f6c74024e4bfecc2845b81ba5746aeb
MORGAN713 <morganazubuike@gmail>
2025-11-11 23:19:02 +0100

    class

---
+++ UsingMathsClass.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class UsingMathsClass{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter any number of your choice:");
+		int num1 = input.nextInt();
+		
+		System.out.print("Enter any number of your choice:");
+		int num2 = input.nextInt();
+		
+		System.out.print("Enter any number to find the squareroot:");
+		int squareRootNum = input.nextInt();
+		
+		int castedSquareRootNum =(int) Math.sqrt(squareRootNum);
+		//output
+		System.out.printf("The absolut value of the number is %d %d%n ",Math.abs(num1),Math.abs(num2));
+		System.out.printf("The maximum number is %d%n ",Math.max(num1,num2));
+		System.out.printf("The minimum number is %d%n ",Math.min(num1,num2));
+		System.out.printf("The square root is %d%n ",castedSquareRootNum);
+		System.out.printf("The answer is %f%n ",Math.pow(num1,2)*Math.pow(num2,2));
+		System.out.printf("The random number is %f%n ",Math.random()*Math.PI());
+	}
+}
