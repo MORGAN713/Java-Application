c555768a233d1e3d358871a1640f03ff084a58c8
MORGAN713 <morganazubuike@gmail>
2025-11-13 11:41:43 +0100

    MethodOverLoading

---
+++ SentinelControlLoop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SentinelControlLoop{
+	 public static void main(String[] args){
+		Scanner input = new Scanner(system.in);
+		
+		while(true){
+		    system.out.print("enter number: ");
+			num = input.nextInt();
+			
+			sum += num;
+			if(num == -1){
+			    break;
+			}
+			
+				
+	 }
+}
+  
